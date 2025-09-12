652dccc9e90ead130d89657e41a4864a85ae2d6b
Art <lyndonehemplo1@gmail.com>
2025-09-12 20:20:10 +0800

    hilt plugin setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("kotlin-kapt") // REQUIRED for Hilt code generation
+    id("dagger.hilt.android.plugin") // REQUIRED for Hilt to work
+    id("kotlin-parcelize")
 }
 
 android {
