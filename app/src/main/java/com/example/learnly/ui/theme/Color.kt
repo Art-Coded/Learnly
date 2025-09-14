98b464c15c0807dee6c776d1ccdacaffe19eac1c
Art <lyndonehemplo1@gmail.com>
2025-09-14 20:35:22 +0800

    themes & drawables setup

--- app/src/main/java/com/example/learnly/ui/theme/Color.kt
+++ app/src/main/java/com/example/learnly/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val blue_green = Color(0xFF167A4E)
+val dark_grey = Color(0xFF142025)
+val calm_white = Color(0xFFDEF4F6)
+
+val grey = Color(0xFF2d3733)
+
+val light_green = Color(0xFF23dC9F)
--- app/src/main/java/com/example/learnly/ui/theme/Theme.kt
+++ app/src/main/java/com/example/learnly/ui/theme/Theme.kt
@@ -12,15 +12,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = calm_white,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    surface = grey,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = blue_green,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    surface = calm_white,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
