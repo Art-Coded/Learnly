74004a9b44eb476ea8e45e3fc5cb3dacb4df118f
Art <lyndonehemplo1@gmail.com>
2025-09-14 20:36:27 +0800

    login screen ui

---
+++ app/src/main/java/com/example/learnly/Login/Login.kt
@@ -0,0 +1,282 @@
+package com.example.learnly.Login
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.learnly.R
+import com.example.learnly.ui.theme.blue_green
+
+@Composable
+fun LoginScreen(homeClick: () -> Unit) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    var hasSubmittedEmail by remember { mutableStateOf(false) }
+    val isEmailValid = remember(email) {
+        email.matches(Regex("^[A-Za-z0-9._%+-]+@(gmail|yahoo)\\.com$"))
+    }
+
+    val passwordFocusRequester = remember { FocusRequester() }
+    var passwordVisible by remember { mutableStateOf(false) }
+
+    val focusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .pointerInput(Unit) {
+            detectTapGestures {
+                focusManager.clearFocus()
+                keyboardController?.hide()
+            }
+        }) {
+        Image(
+            painter = painterResource(id = R.drawable.shapes),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop,
+            alpha = 0.5f
+        )
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.TopStart)
+        ) {
+            IconButton(
+                onClick = {
+
+                },
+                modifier = Modifier
+                    .height(46.dp)
+                    .padding(start = 16.dp, top = 18.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_back),
+                    contentDescription = "Back"
+                )
+            }
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Text(
+                text = "Login with",
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    fontSize =30.sp,
+                    fontWeight = FontWeight.Medium
+                ),
+                color = MaterialTheme.colorScheme.onBackground,
+                modifier = Modifier
+                    .padding(start = 34.dp, top = 16.dp)
+
+            )
+            Row {
+                Text(
+                    text = "Learnly",
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontSize = 48.sp,
+                        fontWeight = FontWeight.ExtraBold
+                    ),
+                    color = MaterialTheme.colorScheme.onBackground,
+                    modifier = Modifier
+                        .padding(start = 34.dp, top = 8.dp)
+
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.schoollogo),
+                    contentDescription = "School Logo",
+                    modifier = Modifier
+                        .height(70.dp)
+                        .padding(start = 8.dp, top = 10.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp)
+            ) {
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = {
+                        email = it
+                        if (hasSubmittedEmail) hasSubmittedEmail = false
+                    },
+                    label = { Text("Email") },
+                    isError = hasSubmittedEmail && !isEmailValid,
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email,
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = {
+                            hasSubmittedEmail = true
+                            if (isEmailValid) {
+                                passwordFocusRequester.requestFocus()
+                            }
+                        }
+                    ),
+                    leadingIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_password),
+                            contentDescription = "Email Icon"
+                        )
+                    },
+                    supportingText = {
+                        Text(
+                            text = if (hasSubmittedEmail && !isEmailValid) "Please enter a valid email." else " ",
+                            color = if (hasSubmittedEmail && !isEmailValid) MaterialTheme.colorScheme.error else Color.Transparent
+                        )
+                    }
+                    ,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+            }
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp, start = 16.dp, end = 16.dp)
+                    .focusRequester(passwordFocusRequester),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done
+                ),
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_password),
+                        contentDescription = "Password Icon"
+                    )
+                },
+                trailingIcon = {
+                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                        Icon(
+                            painter = painterResource(
+                                id = if (passwordVisible) R.drawable.ic_visibility_off else R.drawable.ic_visibility
+                            ),
+                            contentDescription = if (passwordVisible) "Hide password" else "Show password"
+                        )
+                    }
+                }
+            )
+
+
+
+            Spacer(modifier = Modifier.height(6.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 4.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    var rememberMeChecked by remember { mutableStateOf(false) }
+
+                    Checkbox(
+                        checked = rememberMeChecked,
+                        onCheckedChange = { rememberMeChecked = it }
+                    )
+                    Text(text = "Remember me", fontSize = 14.sp)
+                }
+
+                Text(
+                    text = "Forgot Password?",
+                    fontSize = 14.sp,
+                    modifier = Modifier.clickable {
+                        // TODO: Forgot password logic
+                    }
+                )
+            }
+
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = {
+                    homeClick()// TODO: Navigate to Login
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = blue_green,
+                    contentColor = Color.White
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+                    .padding(start = 12.dp, end = 12.dp)
+            ) {
+                Text(text = "Login")
+            }
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = "Don't have an account?",
+                fontSize = 14.sp,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+            Text(
+                text = "Signup",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                color = blue_green,
+                modifier = Modifier
+                    .padding(top = 4.dp)
+                    .clickable {
+
+                    }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Developed by DCODE",
+                fontSize = 14.sp,
+                modifier = Modifier
+                    .alpha(0.5f)
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/learnly/MainActivity.kt
+++ app/src/main/java/com/example/learnly/MainActivity.kt
@@ -10,16 +10,12 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.learnly.Login.LoginScreen
 import com.example.learnly.ui.theme.LearnlyTheme
-import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,44 +25,26 @@ class MainActivity : ComponentActivity() {
             LearnlyTheme {
 
                 val navController = rememberNavController()
-                val startDestination = remember {
-                    val user = FirebaseAuth.getInstance().currentUser
-                    if (user != null && user.isEmailVerified) {
-                        "BottomNav"
-                    } else {
-                        "onboarding"
-                    }
-                }
 
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     NavHost(
                         navController = navController,
-                        startDestination = startDestination,
+                        startDestination = "login",
                         modifier = Modifier.padding(innerPadding),
                         enterTransition = { fadeIn(animationSpec = tween(100)) },
                         exitTransition = { fadeOut(animationSpec = tween(100)) },
                     ) {
-                        composable("onboarding") {
-                            OnboardingScreen(
-                                onLoginClick = { navController.navigate("login") { popUpTo("onboarding") { inclusive = true } } },
-                                signupClick = { navController.navigate("signup") { popUpTo("onboarding") { inclusive = true } } }
-                            )
-                        }
                         composable("login") {
                             LoginScreen(
-                                onBoardingClick = { navController.navigate("onboarding") { popUpTo("login") { inclusive = true } } },
-                                signupClick = { navController.navigate("signup") { popUpTo("login") { inclusive = true } } },
-                                homeClick = { navController.navigate("BottomNav") { popUpTo("login") { inclusive = true } }
+                                homeClick = {
+                                    navController.navigate("BottomNav") {
+                                        popUpTo("login") {
+                                            inclusive = true
+                                        }
+                                    }
                                 }
                             )
                         }
-                        composable("signup") {
-                            SignupScreen(
-                                loginClick = { navController.navigate("login") { popUpTo("signup") { inclusive = true } } },
-                                loginClick1 = { navController.navigate("login") { popUpTo("signup") { inclusive = true } } }
-                            )
-                        }
-                        composable("BottomNav") { BottomNavScreen(rootNavController = navController) }
                     }
                 }
             }
